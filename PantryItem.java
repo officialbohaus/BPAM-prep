a71c46b6f23c3cd274e81aae5e5a6e0418ad0800
officialbohaus <110848971+officialbohaus@users.noreply.github.com>
2025-02-16 09:42:25 -0700

    InitialFileEntry

---
+++ PantryItem.java
@@ -0,0 +1,13 @@
+public interface PantryItem {
+    
+    public void addQty();
+
+    public void removeQty();
+
+    public void setQty(int qty);
+
+    public int getQty();
+
+    public IngredientType getType();
+
+}
---
+++ PantryStorage.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class PantryStorage {
+    
+    private ArrayList<PantryItem> pantryItems = new ArrayList<PantryItem>();
+
+}
+
+
