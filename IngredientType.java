2d21d39ba76d3ae07ad12f232bd99e9673e3ef45
officialbohaus <110848971+officialbohaus@users.noreply.github.com>
2025-02-16 14:31:22 -0700

    Added IngredientType enums

--- IngredientType.java
+++ IngredientType.java
@@ -1,3 +1,16 @@
 public enum IngredientType {
-    
+    Spice("Sp"),
+    Grain("Gr"),
+    Fruit("Fr"),
+    Veggie("Vg"),
+    Protein("Pr"),
+    Dairy("Dy"),
+    Baking("Bk"),
+    Sauce("Sc"),
+    Fat("Fat"),
+    Nut("Nut"),
+    Misc("Msc");
+    private String typeID;
+    private IngredientType(String typeID) { this.typeID = typeID; }
+    public String getTypeID() { return typeID; }
 }
