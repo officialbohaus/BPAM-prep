381b7a8c3e37d85e77a289d914123b1102dc5852
officialbohaus <110848971+officialbohaus@users.noreply.github.com>
2025-02-21 11:17:30 -0700

    played with driver more

--- Driver.java
+++ Driver.java
@@ -1,13 +1,22 @@
 public class Driver {
     public static void main(String[] args) {
         Ingredient apple = new Ingredient("Apple", 0, IngredientType.Fruit, "n/a");
+        Ingredient sugar = new Ingredient("Sugar", 0, IngredientType.Baking, "n.a");
+        
         System.out.println(apple);
+        System.out.println(sugar);
 
         PantryStorage myPantry = new PantryStorage();
 
         myPantry.add(apple);
 
         myPantry.add(apple, 5);
+
+        myPantry.remove(apple, 2);
+
+        myPantry.add(sugar, 200);
+
+        System.out.println(myPantry.contains(sugar));
 
         System.out.println(myPantry);
     }
